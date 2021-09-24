c10ba40245b753b1ccb5da7db6b0020ea7ede407
ferundal <ferundal@gmail.com>
2021-09-24 21:42:59 +0300

    try first bonus

--- src/client_bonus.c
+++ src/client_bonus.c
@@ -1,6 +1,92 @@
 #include "minitalk.h"
 
-int		main()
+int	send_int(int d, pid_t server_pid)
 {
+	int		counter;
+	int 	temp_d;
+
+	counter = 8;
+	while (counter-- > 0)
+	{
+		temp_d = d >> counter;
+		if (temp_d == 0)
+		{
+			if (kill(server_pid, SIGUSR1) != 0)
+				return (1);
+		}
+		else
+		{
+			if (kill(server_pid, SIGUSR2) != 0)
+				return (1);
+			d = d - (temp_d << counter);
+		}
+		usleep(CLIENT_SEND_DEALAY);
+	}
+	return (0);
+}
+
+int	send_char(char c, pid_t server_pid)
+{
+	int		counter;
+	char	temp_c;
+
+	(void)server_pid;
+	counter = 8;
+	while (counter-- > 0)
+	{
+		temp_c = c >> counter;
+		if (temp_c == 0)
+		{
+			if (kill(server_pid, SIGUSR1) != 0)
+				return (1);
+		}
+		else
+		{
+			if (kill(server_pid, SIGUSR2) != 0)
+				return (1);
+			c = c - (temp_c << counter);
+		}
+		usleep(CLIENT_SEND_DEALAY);
+	}
+	return (0);
+}
+
+int	send_str(char *str, pid_t server_pid)
+{
+	while (*str != '\0')
+		if (send_int(*str++, server_pid) != 0)
+			return (1);
+	if (send_int('\n', server_pid) != 0)
+		return (1);
+	return (0);
+}
+
+int	check_args(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		ft_putstr_fd("Wrong arguments amount\n", 2);
+		return (1);
+	}
+	if (ft_isdigit_str(argv[1]) == 0)
+	{
+		ft_putstr_fd("Wrong client_pid\n", 2);
+		return (1);
+	}
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	pid_t	server_pid;
+
+	if (check_args(argc, argv) != 0)
+		return (1);
+	server_pid = ft_atoi(argv[1]);
+	if (send_str(argv[2], server_pid) != 0)
+	{
+		ft_putstr_fd("Error while sending string\n", 2);
+		return (1);
+	}
 	return (0);
 }
--- src/server.c
+++ src/server.c
@@ -1,11 +1,10 @@
 #include "minitalk.h"
 
-
 void	zero_handler(int sig, siginfo_t *info, void *ucontext)
 {
 	(void)sig;
 	(void)ucontext;
-	g_data.client_pid = info->si_pid;
+	(void)info;
 	g_data.value = g_data.value << 1;
 	if (g_data.status == 7)
 	{
@@ -21,7 +20,7 @@ void	one_handler(int sig, siginfo_t *info, void *ucontext)
 {
 	(void)sig;
 	(void)ucontext;
-	g_data.client_pid = info->si_pid;
+	(void)info;
 	g_data.value = g_data.value << 1;
 	++g_data.value;
 	if (g_data.status == 7)
